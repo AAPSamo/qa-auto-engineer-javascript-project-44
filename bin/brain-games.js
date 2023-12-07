c05971343ee5e5d9b774b755e135aa9ce50e403a
John Doe <johndoe@example.com>
2023-12-07 10:13:16 +0300

    izm

---
+++ bin/brain-games.js
@@ -0,0 +1,7 @@
+#!/usr/bin / env node
+
+import readlineSync from 'readline-sync';
+console.log('node bin/brain-games.js')
+console.log('Welcome to the Brain Games!')
+
+const name = readlineSync.question('Your answer: ');
