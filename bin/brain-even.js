1d8e701c60a5b64553c15be8072f0885c2690a24
John Doe <johndoe@example.com>
2023-12-21 10:08:09 +0300

    fix even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 import { name, answer } from '../src/brain-even-logic.js';
-// console.log('node bin/brain-games.js')
+
 console.log('Welcome to the Brain Games!');
 name();
-const answerdigit = [4, 15, 20];
+const answerdigit = [15, 6, 7];
 answer(answerdigit);
--- src/brain-even-logic.js
+++ src/brain-even-logic.js
@@ -12,7 +12,6 @@ export const answer = (questionDigit) => {
 
   let i = 0;
   let countTrueAnswers = 0;
-  // var questionDigit;
 
   for (i; i <= 2; i += 1) {
     if (questionDigit[i] % 2 == 0) {
@@ -22,23 +21,29 @@ export const answer = (questionDigit) => {
         console.log('Correct!');
         countTrueAnswers += 1;
       } else if (answerUser === 'no') {
-        console.log('Error');
-      } else { console.log('Error'); }
+        console.log(`'${answerUser}'` + ' is wrong answer ;(. Correct answer was ' + '"yes"' + '.');
+      console.log(`Let\'s try again, ${nameUser}!`);
+      i = 2;
+      } else { console.log(`'${answerUser}'` + ' is wrong answer ;(. Correct answer was ' + '"yes"' + '.');
+      console.log(`Let\'s try again, ${nameUser}!`);
+      i = 2; }
     } else {
       console.log(`Question: ${questionDigit[i]}`);
       var answerUser = readlineSync.question('Your answer: ');
       if (answerUser === 'yes') {
-        console.log('Error');
+        console.log(`'${answerUser}'` + ' is wrong answer ;(. Correct answer was ' + '"no"' + '.');
+      console.log(`Let\'s try again, ${nameUser}!`);
+      i = 2;
       } else if (answerUser === 'no') {
         console.log('Correct!');
         countTrueAnswers += 1;
-      } else { console.log('Error'); }
+      } else { console.log(`'${answerUser}'` + ' is wrong answer ;(. Correct answer was ' + '"no"' + '.');
+      console.log(`Let\'s try again, ${nameUser}!`);
+      i = 2; }
     }
   }
-
   if (countTrueAnswers == 3) {
     console.log(`Congratulations, ${nameUser}!`);
-  } else {
-    console.log(`You have errors, ${nameUser}!`);
-  }
+  } 
+  
 };
