e6585fd8bb10305d1296af9b125699f7a598de05
John Doe <johndoe@example.com>
2023-12-21 16:52:12 +0300

    fix

--- src/brain-even-logic.js
+++ src/brain-even-logic.js
@@ -9,44 +9,44 @@ export const name = () => {
 
 export const answer = (questionDigit) => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
   let i = 0;
   let countTrueAnswers = 0;
 
   for (i; i <= 2; i += 1) {
-    if (questionDigit[i] % 2 == 0) {
+    let answerUser;
+    if (questionDigit[i] % 2 === 0) {
       console.log(`Question: ${questionDigit[i]}`);
-      var answerUser = readlineSync.question('Your answer: ');
+      answerUser = readlineSync.question('Your answer: ');
       if (answerUser === 'yes') {
         console.log('Correct!');
         countTrueAnswers += 1;
       } else if (answerUser === 'no') {
-        console.log(`'${answerUser}'` + ' is wrong answer ;(. Correct answer was ' + '"yes"' + '.');
-        console.log(`Let\'s try again, ${nameUser}!`);
+        console.log(`'${answerUser}' is wrong answer ;(. Correct answer was "yes".`);
+        console.log(`Let's try again, ${nameUser}!`);
         i = 2;
       } else {
-        console.log(`'${answerUser}'` + ' is wrong answer ;(. Correct answer was ' + '"yes"' + '.');
-        console.log(`Let\'s try again, ${nameUser}!`);
+        console.log(`'${answerUser}' is wrong answer ;(. Correct answer was "yes".`);
+        console.log(`Let's try again, ${nameUser}!`);
         i = 2;
       }
     } else {
       console.log(`Question: ${questionDigit[i]}`);
-      var answerUser = readlineSync.question('Your answer: ');
+      answerUser = readlineSync.question('Your answer: ');
       if (answerUser === 'yes') {
-        console.log(`'${answerUser}'` + ' is wrong answer ;(. Correct answer was ' + '"no"' + '.');
-        console.log(`Let\'s try again, ${nameUser}!`);
+        console.log(`'${answerUser}' is wrong answer ;(. Correct answer was "no".`);
+        console.log(`Let's try again, ${nameUser}!`);
         i = 2;
       } else if (answerUser === 'no') {
         console.log('Correct!');
         countTrueAnswers += 1;
       } else {
-        console.log(`'${answerUser}'` + ' is wrong answer ;(. Correct answer was ' + '"no"' + '.');
-        console.log(`Let\'s try again, ${nameUser}!`);
+        console.log(`'${answerUser}' is wrong answer ;(. Correct answer was "no".`);
+        console.log(`Let's try again, ${nameUser}!`);
         i = 2;
       }
     }
   }
-  if (countTrueAnswers == 3) {
+  if (countTrueAnswers === 3) {
     console.log(`Congratulations, ${nameUser}!`);
   }
 };
